da4d1123e041ffd73faa22b1012e241ad46823cf
hyk2197696 <hyk2197696@163.com>
2017-12-18 13:27:59 +1100

    no added

--- controllers/contentcreatecontroller.js
+++ controllers/contentcreatecontroller.js
@@ -23,6 +23,11 @@ exports.content_create_post = function(req, res, next){
 
     query.on('end',function(){
             //if insert successfully jump to the success page
-            res.render('homepage',{title:'New ' + content_name + ' creation success'});
+            //res.render('homepage',{title:'New ' + content_name + ' creation success'});
         })
 };
+
+exports.content_create_modal_post = function(req, res, next){
+    req.checkBody('content_name', content_name + ' name must be specified.').notEmpty();
+    req.sanitize('content_name').escape();
+}
--- controllers/directorycontroller.js
+++ controllers/directorycontroller.js
@@ -6,6 +6,12 @@ var con = require('./databasecontroller')
 var app = require('../app')
 var json = require('json')
 
+var directory = '';
+var id = '';
+
+
+
+
 exports.find_get = function(req, res, next) {
     var sql = 'select * from sys.fakedirectory where super';
 
@@ -58,7 +64,6 @@ exports.check_folder_existance = function (req, res, next ){
     query = con.query(sql);
     query.on('result',function(row){
         if(row.count == '0'){
-
             sql = 'insert into sys.fakedirectory values (default, \'' + req.query.name + '\' , ';
             if(req.query.id == 0){
                 sql += 'null)';
@@ -79,26 +84,112 @@ exports.check_folder_existance = function (req, res, next ){
     //res.end('success');
 };
 
-exports.get_full_folder_directory = function (req,res,next){
-    var directory = '';
-    var id = req.query.id;
-    sql = 'select name, ifnull(super,\'null\'\) as super from sys.fakedirectory where idfakedirectory =' +  req.query.id;
+function get_folder_info(){
+    var sql = 'select name,ifnull(super,\'null\') as super from sys.fakedirectory where idfakedirectory = '+ id;
+    console.log('new query :' + sql);
+    var query = con.query(sql);
+    var name,super_id;
+    query.on('result', function(row) {
+        name = row.name.toString();
+        super_id = row.super.toString();
+    });
+    query.on('end',function(){
+        id = super_id;
+        directory = name + '/' + directory;
+    })
+};
+
+
+
+exports.get_full_folder_directory = function (req,res,next) {
+    id = req.query.id;
+    var sql = 'select name, ifnull(super,\'null\'\) as super from sys.fakedirectory where idfakedirectory =' + req.query.id;
     console.log("new query" + sql);
-    con.query(sql).on('result',function(row){
+    var query = con.query(sql);
+
+    query.on('result', function (row) {
         console.log(row);
         directory = row.name.toString();
         id = row.super.toString();
-
     });
-    while(id != 'null'){
-        sql = 'select name,ifnull(super,\'null\') as super from sys.fakedirectory where idfakedirectory = '+ id;
-        console.log('new query :' + sql);
-        con.query(sql).on('result', function(){
-            directory = row.name.toString() + '/' + directory;
-            id = row.super.toString();
-        })
+    query.on('end', function () {
+        while (id != 'null') {
+            get_folder_info(id);
+        }
+        res.end(directory);
+    });
+};
+
+    // while(id != 'null'){
+    //     sql = 'select name,ifnull(super,\'null\') as super from sys.fakedirectory where idfakedirectory = '+ id;
+    //     console.log('new query :' + sql);
+    //     con.query(sql).on('result', function(){
+    //         directory = row.name.toString() + '/' + directory;
+    //         id = row.super.toString();
+    //     })
+    //
+    //
+    // }
+    // res.end(directory);
+    // ;
 
 
-    }
-    res.end(directory);
-}
+
+//
+// console.log('directory before = ' + directory);
+//
+// // get_folder_info is called synchronously, but the 2nd argument here is a callback function
+// // this callback function is called via the query 'end' event listener
+// // event listeners are asynchronous, and as we don't know how the query 'end' event is handled internally,
+// // we can't guarantee execution order. this means we need to shuffle our code a bit to get the desired result.
+// // basically, think of your current code execution as...
+// // get_folder_info call -> res.end -> get_folder_info calls call_back when query finally fires 'end'
+// get_folder_info(id, function(name, super_id) {
+//     directory = name + '/' + directory;
+//     console.log('directory inside = ' + directory);
+//     id = super_id;
+// });
+//
+// res.end(directory);
+//
+// console.log('directory after = ' + directory);qq
+//
+// function get_folder_info(id, call_back) {
+//     var sql =
+//         "select name,ifnull(super,'null') as super from sys.fakedirectory where idfakedirectory = " +
+//         id;
+//
+//     console.log('new query :' + sql);
+//
+//     var query = con.query(sql);
+//     var name, super_id;
+//
+//     query.on('result', function(row) {
+//         name = row.name.toString();
+//         super_id = row.super.toString();
+//     });
+//
+//     query.on('end', function() {
+//         call_back(name, super_id);
+//     });
+// }
+//
+// // fixed order
+// // you can see that res.end and the console.log have been shifted to the end of the callback function
+// // this is so that get_folder_info will execute, then finally when the query 'end' event fires
+// // at an undetermined time due to it's asynchronous nature, your response will be closed _afterwards_
+// console.log('before');
+//
+// get_folder_info(id, function(name, superId) {
+//
+//     // ...
+//
+//     res.end(directory);
+//
+//     console.log('after');
+// });
+//
+//
+// function get_folder_info(id, call_back) {
+//     // ...
+//  }
